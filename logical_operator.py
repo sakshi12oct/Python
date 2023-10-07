1bdb47b45a2fd70034311e27084e29e58b70548b
sakshi12oct <sakshisaxena12oct@gmail.com>
2023-10-07 10:52:24 +0530

    Create logical_operator.py

---
+++ logical_operator.py
@@ -0,0 +1,19 @@
+# Logical AND operator
+x = True
+y = False
+
+# AND operator returns True only if both operands are True
+result_and = x and y
+print("x and y:", result_and)  # Output: x and y: False
+
+# Logical OR operator
+# OR operator returns True if at least one operand is True
+result_or = x or y
+print("x or y:", result_or)    # Output: x or y: True
+
+# Logical NOT operator
+# NOT operator negates the Boolean value
+result_not_x = not x
+result_not_y = not y
+print("not x:", result_not_x)  # Output: not x: False
+print("not y:", result_not_y)  # Output: not y: True
