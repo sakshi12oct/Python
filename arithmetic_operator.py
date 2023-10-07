694d29c0de9f8976519f2536fae36cdbb62d91c6
sakshi12oct <sakshisaxena12oct@gmail.com>
2023-10-07 10:36:30 +0530

    Create arithmetic_operator.py

---
+++ arithmetic_operator.py
@@ -0,0 +1,29 @@
+# Addition
+a = 5
+b = 3
+sum_result = a + b
+print("Addition:", sum_result)  # Output: Addition: 8
+
+# Subtraction
+difference_result = a - b
+print("Subtraction:", difference_result)  # Output: Subtraction: 2
+
+# Multiplication
+product_result = a * b
+print("Multiplication:", product_result)  # Output: Multiplication: 15
+
+# Division (results in a float)
+division_result = a / b
+print("Division:", division_result)  # Output: Division: 1.6666666666666667
+
+# Floor Division (results in an integer, discards decimal)
+floor_division_result = a // b
+print("Floor Division:", floor_division_result)  # Output: Floor Division: 1
+
+# Modulus (remainder of division)
+modulus_result = a % b
+print("Modulus:", modulus_result)  # Output: Modulus: 2
+
+# Exponentiation
+exponent_result = a ** b
+print("Exponentiation:", exponent_result)  # Output: Exponentiation: 125
